820b7c93ae989805aef449a5f598c3d38b75b43d
Danilo Herrera <daniloherrera1702@gmail.com>
2021-10-12 12:00:22 -0500

    Comentarios para el mini-jueguito

--- main.c
+++ main.c
@@ -4,15 +4,19 @@
 
 int main()
 {
+    //Variables.
     int numA, numB;
 
+    //Hacemos que la maquina nos de un numero aleatorio entre 1-10.
     srand(time(NULL));
     numA = rand() % 10 + 1;
 
+    //Preguntamos que valor va a elgir para jugar.
     printf("Dame un numero entero entre 1-10 \n");
     printf("\n \n");
     scanf("%i", &numB);
 
+    //condiciones para saber que resultado mostrar al jugador.
     if(numA == numB)
     {
         printf("FELICIDADES acertaste el numero fue: %d", numA);
